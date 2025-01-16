a959c73d65f2511cc12b4cc99b2a63e673b9c4e3
rogilmore <rog1@psu.edu>
2025-01-16 12:37:08 -0500

    Fix broken link to video in 2025-01-16 slides; flesh-out workflow for psych-sci-figs.qmd; fix PDF rendering bug in _about

--- src/R/make_iframe_from_url.R
+++ src/R/make_iframe_from_url.R
@@ -19,3 +19,31 @@ return_iframe_chunk <- function(data, var) {
     quiet = TRUE
   )
 }
+
+make_img_markdown_from_url <- function(img_url = "https://www.psychologicalscience.org/redesign/wp-content/uploads/2015/12/TCD_fig_1.jpg") {
+  assertthat::is.string(img_url)
+  assertthat::assert_that(stringr::str_detect(img_url, "^https://"))
+  
+  component_1 <- paste0("![](")
+  component_2 <- paste0(img_url, ")")
+  paste0(component_1, component_2)
+}
+
+
+return_img_chunk <- function(i, df) {
+  df_i <- df[i,]
+  chunk_hdr <- knitr::knit_expand(text = c("### Figure from: ",df_i$url_to_src, "\n\n"))
+  if (!is.na(df_i$url_to_figure)) {
+    img_markdown <- make_img_markdown_from_url(df_i$url_to_figure)
+  } else {
+    img_markdown = "**No direct link to figure**"
+  }
+  body_txt <- paste0("**Analyst**: ", df_i$identifier, " | **Type**: ", df_i$source_type, "\n")
+  selected_txt <- paste0("**Why selected**: ", df_i$why_selected)
+  
+  knitr::knit_child(
+    text = c(chunk_hdr, img_markdown, body_txt, selected_txt),
+    envir = environment(),
+    quiet = TRUE
+  )  
+}
