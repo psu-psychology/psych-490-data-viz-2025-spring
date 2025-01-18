0d4ca3ed584c4b5cbe65c1c026c65516b09202ee
rogilmore <rog1@psu.edu>
2025-01-18 09:42:24 -0500

    More updates to psych-sci-figs.qmd; add link to this page in slides and render.

---
+++ src/R/make_img_markdown_from_url.R
@@ -0,0 +1,8 @@
+make_img_markdown_from_url <- function(img_url = "https://www.psychologicalscience.org/redesign/wp-content/uploads/2015/12/TCD_fig_1.jpg") {
+  assertthat::is.string(img_url)
+  assertthat::assert_that(stringr::str_detect(img_url, "^https://"))
+  
+  component_1 <- paste0("![](")
+  component_2 <- paste0(img_url, ")")
+  paste0(component_1, component_2)
+}
