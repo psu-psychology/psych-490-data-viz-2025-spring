661d36cfa77529d66fb9fbf4c6bd8bc94092387e
rogilmore <rog1@psu.edu>
2025-01-29 12:16:25 -0500

    Fix broken links from Google sheet used in ex02 by editing CSV directly.

--- src/R/make_img_markdown_from_url.R
+++ src/R/make_img_markdown_from_url.R
@@ -1,7 +1,7 @@
 make_img_markdown_from_url <- function(img_url = "https://www.psychologicalscience.org/redesign/wp-content/uploads/2015/12/TCD_fig_1.jpg",
                                        lightbox = "{.lightbox}") {
   assertthat::is.string(img_url)
-  assertthat::assert_that(stringr::str_detect(img_url, "^https://"))
+  #assertthat::assert_that(stringr::str_detect(img_url, "^https://"))
   
   component_1 <- paste0("![](")
   component_2 <- paste0(img_url, ")", lightbox)
